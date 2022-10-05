1f0847152727699a3e513fc80cb8dc7c26bc97b1
Crystal Goodwin <sgcg03@mac.com>
2022-10-04 20:12:20 -0400

    Coding challenge Add Item to Array

---
+++ 8kyu/add-item-to-array/app.js
@@ -0,0 +1,6 @@
+//Add the value "codewars" to the websites array. After your code executes the websites array should == ["codewars"]
+
+let websites = [];
+websites.push("codewars");
+
+console.log(websites[0]);
