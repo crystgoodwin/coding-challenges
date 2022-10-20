a685224b137c5b4ff9472d071860b0de80e08200
Crystal Goodwin <sgcg03@mac.com>
2022-10-19 20:21:12 -0400

    Coding challenge 10-19-22 Remove first and last character

---
+++ 8kyu/remove-first-last-char/app.js
@@ -0,0 +1,11 @@
+//Your goal is to create a function that removes the first and last characters of a string. You're given one parameter, the original string. You don't have to worry with strings with less than two characters.
+
+function removeChar(str){
+  return str.slice(1, -1);
+
+}
+console.log(removeChar('Hello World'));
+
+//Arrow function
+const removedChar = str => str.slice(1, -1);
+console.log(removedChar('Character'));
