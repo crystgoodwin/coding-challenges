88a6bfe5f43f5776897a4c2e48621128a6c76300
Crystal Goodwin <sgcg03@mac.com>
2022-10-25 20:21:12 -0400

    Coding challenge 10-25-22 Cound odd numbers below n

---
+++ 8kyu/count-odd-numbers.js
@@ -0,0 +1,8 @@
+// Given a number n, return the number of positive odd numbers below n.
+
+const oddCount = (n) => Math.floor(n / 2);
+
+//alternate
+function oddCount1(x){
+   return Math.floor(x/2);
+}
